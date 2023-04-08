cc3aa7f929c28300dd2766b6ef9d87a6162bc874
Devon Wells <devon.wellsa@gmail.com>
2023-04-08 14:20:44 -0400

    fix parcel

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Button, Card } from "example";
+import { Button } from "example";
 
 export default function RootPage() {
   return (
     <div>
       <h1 className="text-5xl font-bold">Template Page</h1>
       <Button />
-      <Card cta="hello" title="world" href="www.google.com" />
     </div>
   );
 }
--- packages/components/example/src/Button.tsx
+++ packages/components/example/src/Button.tsx
@@ -4,9 +4,9 @@ export const Button = () => {
   return (
     <div className="rounded-md ">
       <a href="https://turbo.build/repo/docs">
-        <div className="ui-flex ui-w-full ui-items-center ui-justify-center ui-rounded-md ui-border ui-border-transparent ui-px-8 ui-py-3 ui-text-base ui-font-medium ui-no-underline ui-bg-white ui-text-black hover:ui-bg-gray-300 md:ui-py-3 md:ui-px-10 md:ui-text-lg md:ui-leading-6">
-          <span className="text-red-500">Read the docs</span>
-          <span className="ui-ml-2 ui-bg-gradient-to-r ui-from-brandred ui-to-brandblue ui-bg-clip-text ui-text-transparent">
+        <div className="flex w-full items-center justify-center rounded-md border border-transparent px-8 py-3 text-base font-medium no-underline bg-white text-black hover:bg-gray-300 md:py-3 md:px-10 md:text-lg md:leading-6">
+          <span className="text-violet-900">Read the docs</span>
+          <span className="ml-2 bg-gradient-to-r from-red-500 to-blue-500 bg-clip-text text-transparent">
             →
           </span>
         </div>
